c81cedab81901d475b549e22e752ef5631d5f0c3
dannab <koogoori54@gmail.com>
2024-06-24 01:48:33 +0900

    :tada: project setting

---
+++ src/main/java/org/pp/petionary/PetionaryApplication.java
@@ -0,0 +1,13 @@
+package org.pp.petionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetionaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pp/petionary/PetionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pp.petionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
